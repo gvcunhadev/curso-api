4aabbe9504030ba8f432b469b7a930a48902c0a4
gvcunhadev <gabiviana.cunha@gmail.com>
2024-04-26 10:35:17 -0300

    tratamento de excecao para objeto nao encontrado

---
+++ src/main/java/br/com/dicasdeumdev/api/resources/exceptions/StandartError.java
@@ -0,0 +1,20 @@
+package br.com.dicasdeumdev.api.resources.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class StandartError {
+
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String path;
+}
