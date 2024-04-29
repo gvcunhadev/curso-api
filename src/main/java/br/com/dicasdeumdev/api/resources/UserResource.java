ebd2252ef84a7252fcc4c053dfd7bd0f94498132
gvcunhadev <gabiviana.cunha@gmail.com>
2024-04-29 10:45:29 -0300

    excecao para email ja cadastrado na atualizacao

--- src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -1,6 +1,5 @@
 package br.com.dicasdeumdev.api.resources;
 
-import br.com.dicasdeumdev.api.domain.User;
 import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.services.UserService;
 import org.modelmapper.ModelMapper;
